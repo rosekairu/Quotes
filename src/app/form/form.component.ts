a3bf2eb73ebe889c140178c1f4e3af1a8e80b4ab
Rose Kairu <rosekairu@gmail.com>
2020-05-08 23:35:11 +0300

    modified typescript form file

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  newEntry = new Quotes('', '', '');
+  @Output() emitQuote = new EventEmitter();
+  quoteWords: string;
+  quoteEditor: string;
+  quoteAuthor: string;
+  quoteDate: number;
+  theWords: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitQuote() {
+    this.theWords = new Quotes(
+      this.quoteWords,
+      this.quoteEditor,
+      this.quoteAuthor
+    );
+    this.quoteWords = '';
+    this.quoteEditor = '';
+    this.quoteAuthor = '';
+    this.emitQuote.emit(this.theWords);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
