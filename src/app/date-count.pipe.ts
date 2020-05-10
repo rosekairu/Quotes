b0036f0d8e5b71c66dde3f793e392231761e3904
Rose Kairu <rosekairu@gmail.com>
2020-05-10 15:16:03 +0300

    Got the date count to work

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -16,7 +16,7 @@ export class DateCountPipe implements PipeTransform {
     var dateDifferenceSeconds = dateDifference * 0.001; //converts miliseconds to seconds
     var dateCounter = Math.round(dateDifferenceSeconds / secondsInADay);
 
-    if (dateCounter >= 1 && value > todayWithNoTime) {
+    if (dateCounter > 1) {
       return dateCounter;
     } else {
       return 0;
